733e4229ea4c5a42b589e5472e41bad7e2bd1b9f
Canberk Cinar <canberkcnr@gmail.com>
2023-03-17 12:02:30 +0300

    Detect, translator eklendi

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from googletrans import Translator
+from langdetect import detect, detect_langs
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -23,25 +25,39 @@ def index():
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-@app.route("/dalle", methods = ("GET", "POST"))
-def dalle(image):
-    if request.method == "POST":
-        image =  request.form["image"]
-        response = openai.Image.create(
-            prompt=image,
-            n=1,
-            size="1024x1024"
-            )
-        
-        image_url = response['data'][0]['url']
-        
-        return redirect(url_for("dalle", image = image_url))
     
-    image = request.args.get("image")
-    return render_template("dall_e.hthml", image = image)
+    result = detect(result)
+    
+    translator = Translator()
+    
+    if result == "en":
+        
+        result = translator.translate(result)
+
+        return render_template("index.html", result=result)
+    
+    else:
+        
+        return render_template("index.html", result=result)
+    
+
+
+# @app.route("/dalle", methods = ("GET", "POST"))
+# def dalle(image):
+#     if request.method == "POST":
+#         image =  request.form["image"]
+#         response = openai.Image.create(
+#             prompt=image,
+#             n=1,
+#             size="1024x1024"
+#             )
+        
+#         image_url = response['data'][0]['url']
+        
+#         return redirect(url_for("dalle", image = image_url))
+    
+#     image = request.args.get("image")
+#     return render_template("dall_e.hthml", image = image)
 
 def generate_prompt(question):
         return """{}
